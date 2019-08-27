782bcc6f3a5f9be40962cba2dfa17b2848346f5d
alanraul <raulumb@gmail.com>
2019-08-27 10:37:43 -0500

    Confirmación inicial

---
+++ lib/kolombia.ex
@@ -0,0 +1,18 @@
+defmodule Kolombia do
+  @moduledoc """
+  Documentation for Kolombia.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kolombia.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kolombia_test.exs
@@ -0,0 +1,8 @@
+defmodule KolombiaTest do
+  use ExUnit.Case
+  doctest Kolombia
+
+  test "greets the world" do
+    assert Kolombia.hello() == :world
+  end
+end
